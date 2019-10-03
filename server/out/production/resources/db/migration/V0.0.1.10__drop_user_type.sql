d979b9379f1931b6e1c3eceef7eef6c2606da940
Jason Young <thinkbigthings@gmail.com>
2019-10-03 08:52:13 -0400

    updated lib versions

--- server/out/production/resources/db/migration/V0.0.1.10__drop_user_type.sql
+++
@@ -1,6 +0,0 @@
-
-set search_path TO public;
-
-ALTER TABLE users DROP COLUMN type_name;
-
-DROP TABLE user_type;
--- server/out/production/resources/db/migration/V0.0.1.12__rename_users_table.sql
+++
@@ -1,6 +0,0 @@
-
-set search_path TO public;
-
-ALTER TABLE users RENAME TO app_user;
-
-CREATE VIEW users AS (SELECT * FROM app_user) WITH CASCADED CHECK OPTION;
