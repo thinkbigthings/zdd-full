48229e30331a17a6e6933eb7b1e7e7fe52952d64
Jason Young <thinkbigthings@gmail.com>
2019-10-27 12:32:05 -0400

    set data with fetch on first render and on button click

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -7,23 +7,28 @@ function App() {
 
     const [info, setInfo] = useState({users:{count:0}});
 
-    let callForInfo = () => {
+    let fetchData2 = () => {
         fetch('/actuator/info')
             .then(httpResponse => httpResponse.json())
             .then(count => setInfo(count));
     };
 
-    // TODO populate with real data from server on page load
-    // useEffect() ?
-    // callForInfo() ?
-    // call first and pass in as initial value to useState() ?
+    // this works too
+    async function fetchData() {
+        const res = await fetch('/actuator/info');
+        const json = await res.json();
+        setInfo(json);
+    }
+
+    // When React's Suspense feature with fetch is ready, that’ll be the preferred way to fetch data
+    useEffect(fetchData2, [setInfo]);
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <h1 className="App-title">{info.users.count}</h1>
-                <button onClick={callForInfo}>
+                <button onClick={fetchData2}>
                     Click me
                 </button>
             </header>
