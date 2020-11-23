96f21b3b1ff36d026d331cdb93dc03e98a0b2b78
Jason Young <thinkbigthings@gmail.com>
2020-11-23 05:14:06 -0500

    parse item, build in memory data structures

---
+++ server/src/main/java/org/thinkbigthings/zdd/server/scraper/keystone/TerpeneAmount.java
@@ -0,0 +1,7 @@
+package org.thinkbigthings.zdd.server.scraper.keystone;
+
+import java.math.BigDecimal;
+
+public record TerpeneAmount(Terpene terpene, BigDecimal amount) {
+
+}
---
+++ server/src/test/java/org/thinkbigthings/zdd/server/scraper/keystone/ExtractorTest.java
@@ -0,0 +1,40 @@
+package org.thinkbigthings.zdd.server.scraper.keystone;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExtractorTest {
+
+    private Extractor extractor = new Extractor();
+
+    @Test
+    public void testExtractor() throws IOException {
+
+        Path path = Paths.get("src", "test", "resources", "devon-flower.json");
+        String content = Files.readString(path, StandardCharsets.UTF_8);
+
+        List<Item> items = extractor.extractItems(content);
+
+        assertEquals(35, items.size());
+        System.out.println(items);
+
+//        Store website and location
+//        User profile: saved searches,
+
+//        - assemble DB
+//        - IT access DB, be able to sort by selection
+
+
+    }
+
+
+
+}
